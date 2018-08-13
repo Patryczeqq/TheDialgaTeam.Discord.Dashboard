d281f51426238c8bdedd13b090da904641e7e1ac
jianmingyong <jianming1993@gmail.com>
2018-08-13 16:51:17 +0800

    Include ignored files

---
+++ vendor/zendframework/zend-expressive-session-ext/CHANGELOG.md
@@ -0,0 +1,204 @@
+# Changelog
+
+All notable changes to this project will be documented in this file, in reverse chronological order by release.
+
+## 1.1.1 - 2018-05-14
+
+### Added
+
+- Nothing.
+
+### Changed
+
+- Nothing.
+
+### Deprecated
+
+- Nothing.
+
+### Removed
+
+- Nothing.
+
+### Fixed
+
+- [#21](https://github.com/zendframework/zend-expressive-session-ext/pull/21) fixes a situation whereby during persistence, if no identifier existed for
+  the session, it was not persisted. Such situations would occur when a new session was created, as
+  no identifier would yet exist. It now properly generates an identifier and persists the data in
+  such cirumstances.
+
+## 1.1.0 - 2018-05-10
+
+### Added
+
+- Nothing.
+
+### Changed
+
+- [#12](https://github.com/zendframework/zend-expressive-session-ext/pull/12) updates the `PhpSessionPersistence` class such that it is now responsible for
+  emitting the various cache limiter headers (`Expires`, `Cache-Control`, `Last-Modified`, and `Pragma`) normally
+  emitted by ext-session and controlled by the `session.cache_limiter` and `session.cache_expire` INI settings.
+  This approach ensures that those headers are not overwritten by ext-session if set elsewhere in your
+  application.
+
+- [#9](https://github.com/zendframework/zend-expressive-session-ext/pull/9) swaps a call to `session_commit` to `session_write_close` withing `PhpSessionPersistence`,
+  as the former is an alias for the latter.
+
+### Deprecated
+
+- Nothing.
+
+### Removed
+
+- Nothing.
+
+### Fixed
+
+- [#13](https://github.com/zendframework/zend-expressive-session-ext/pull/13) fixes an issue whereby a new session cookie is not always sent
+  following an ID regeneration.
+
+## 1.0.1 - 2018-03-15
+
+### Added
+
+- Nothing.
+
+### Changed
+
+- Nothing.
+
+### Deprecated
+
+- Nothing.
+
+### Removed
+
+- Nothing.
+
+### Fixed
+
+- [#8](https://github.com/zendframework/zend-expressive-session-ext/pull/8)
+  fixes how session resets occur, ensuring cookies are reset correctly.
+
+## 1.0.0 - 2018-03-15
+
+### Added
+
+- Nothing.
+
+### Changed
+
+- Nothing.
+
+### Deprecated
+
+- Nothing.
+
+### Removed
+
+- Removes support for pre-stable 1.0.0 versions of zend-expressive-session.
+
+### Fixed
+
+- Nothing.
+
+## 0.1.4 - 2018-02-28
+
+### Added
+
+- Nothing.
+
+### Changed
+
+- Nothing.
+
+### Deprecated
+
+- Nothing.
+
+### Removed
+
+- Nothing.
+
+### Fixed
+
+- [#1](https://github.com/zendframework/zend-expressive-session-ext/pull/1)
+  fixes a problem that occurs when a requested session does not resolve to an
+  existing session and/or an existing session with empty data, leading to
+  creation of new sessions on each request.
+
+## 0.1.3 - 2018-02-24
+
+### Added
+
+- [#5](https://github.com/zendframework/zend-expressive-session-ext/pull/5) adds
+  support for the ^1.0.0alpha1 release of zend-expressive-session.
+
+## 0.1.2 - 2017-12-12
+
+### Added
+
+- [#3](https://github.com/zendframework/zend-expressive-session-ext/pull/3) adds
+  support for the 1.0-dev and 1.0 releases of zend-expressive-session.
+
+### Changed
+
+- Nothing.
+
+### Deprecated
+
+- Nothing.
+
+### Removed
+
+- Nothing.
+
+### Fixed
+
+- Nothing.
+
+## 0.1.1 - 2017-10-10
+
+### Added
+
+- Nothing.
+
+### Changed
+
+- Nothing.
+
+### Deprecated
+
+- Nothing.
+
+### Removed
+
+- Nothing.
+
+### Fixed
+
+- Fixes session regeneration under PHP 7.2 so that it will not raise warnings.
+
+## 0.1.0 - 2017-10-10
+
+Initial release.
+
+### Added
+
+- Everything.
+
+### Changed
+
+- Nothing.
+
+### Deprecated
+
+- Nothing.
+
+### Removed
+
+- Nothing.
+
+### Fixed
+
+- Nothing.
---
+++ vendor/zendframework/zend-expressive-session-ext/src/PhpSessionPersistence.php
@@ -0,0 +1,245 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-expressive-session-ext for the canonical source repository
+ * @copyright Copyright (c) 2017-2018 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive-session-ext/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Zend\Expressive\Session\Ext;
+
+use Dflydev\FigCookies\FigRequestCookies;
+use Dflydev\FigCookies\FigResponseCookies;
+use Dflydev\FigCookies\SetCookie;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Expressive\Session\Session;
+use Zend\Expressive\Session\SessionInterface;
+use Zend\Expressive\Session\SessionPersistenceInterface;
+
+use function array_merge;
+use function bin2hex;
+use function filemtime;
+use function gmdate;
+use function ini_get;
+use function is_file;
+use function random_bytes;
+use function session_id;
+use function session_name;
+use function session_start;
+use function session_write_close;
+use function sprintf;
+use function time;
+
+/**
+ * Session persistence using ext-session.
+ *
+ * Adapts ext-session to work with PSR-7 by disabling its auto-cookie creation
+ * (`use_cookies => false`), while simultaneously requiring cookies for session
+ * handling (`use_only_cookies => true`). The implementation pulls cookies
+ * manually from the request, and injects a `Set-Cookie` header into the
+ * response.
+ *
+ * Session identifiers are generated using random_bytes (and casting to hex).
+ * During persistence, if the session regeneration flag is true, a new session
+ * identifier is created, and the session re-started.
+ */
+class PhpSessionPersistence implements SessionPersistenceInterface
+{
+    /** @var string|null */
+    private $cookie;
+
+    /** @var string */
+    private $cacheLimiter;
+
+    /** @var int */
+    private $cacheExpire;
+
+    /** @var string */
+    private $scriptFile;
+
+    /** @var array */
+    private static $supported_cache_limiters = [
+        'nocache'           => true,
+        'public'            => true,
+        'private'           => true,
+        'private_no_expire' => true,
+    ];
+
+    /**
+     * This unusual past date value is taken from the php-engine source code and
+     * used "as is" for consistency.
+     */
+    public const CACHE_PAST_DATE  = 'Thu, 19 Nov 1981 08:52:00 GMT';
+
+    public const HTTP_DATE_FORMAT = 'D, d M Y H:i:s T';
+
+    /**
+     * Memoize session ini settings before starting the request.
+     *
+     * The cache_limiter setting is actually "stolen", as we will start the
+     * session with a forced empty value in order to instruct the php engine to
+     * skip sending the cache headers (this being php's default behaviour).
+     * Those headers will be added programmatically to the response along with
+     * the session set-cookie header when the session data is persisted.
+     */
+    public function __construct()
+    {
+        $this->cacheLimiter = ini_get('session.cache_limiter');
+        $this->cacheExpire  = (int) ini_get('session.cache_expire');
+    }
+
+    public function initializeSessionFromRequest(ServerRequestInterface $request) : SessionInterface
+    {
+        $this->scriptFile = $request->getServerParams()['SCRIPT_FILENAME'] ?? __FILE__;
+        $this->cookie = FigRequestCookies::get($request, session_name())->getValue();
+        $id = $this->cookie ?: $this->generateSessionId();
+        $this->startSession($id);
+        return new Session($_SESSION);
+    }
+
+    public function persistSession(SessionInterface $session, ResponseInterface $response) : ResponseInterface
+    {
+        // Regenerate if the session is marked as regenerated
+        // Regenerate if there is no cookie id set but the session has changed (new session with data)
+        if ($session->isRegenerated()
+            || (! $this->cookie && $session->hasChanged())
+        ) {
+            $this->regenerateSession();
+        }
+
+        $_SESSION = $session->toArray();
+        session_write_close();
+
+        if ($this->cookie) {
+            $sessionCookie = SetCookie::create(session_name())
+                ->withValue($this->cookie)
+                ->withPath(ini_get('session.cookie_path'));
+
+            $response = FigResponseCookies::set($response, $sessionCookie);
+
+            if (! $this->cacheLimiter || $this->responseAlreadyHasCacheHeaders($response)) {
+                return $response;
+            }
+
+            $cacheHeaders = $this->generateCacheHeaders();
+            foreach ($cacheHeaders as $name => $value) {
+                if (false !== $value) {
+                    $response = $response->withHeader($name, $value);
+                }
+            }
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param array $options Additional options to pass to `session_start()`.
+     */
+    private function startSession(string $id, array $options = []) : void
+    {
+        session_id($id);
+        session_start(array_merge([
+            'use_cookies'      => false,
+            'use_only_cookies' => true,
+            'cache_limiter'    => '',
+        ], $options));
+    }
+
+    /**
+     * Regenerates the session safely.
+     *
+     * @link http://php.net/manual/en/function.session-regenerate-id.php (Example #2)
+     */
+    private function regenerateSession() : void
+    {
+        session_write_close();
+        $this->cookie = $this->generateSessionId();
+        $this->startSession($this->cookie, [
+            'use_strict_mode' => false,
+        ]);
+    }
+
+    /**
+     * Generate a session identifier.
+     */
+    private function generateSessionId() : string
+    {
+        return bin2hex(random_bytes(16));
+    }
+
+    /**
+     * Generate cache http headers for this instance's session cache_limiter and
+     * cache_expire values
+     */
+    private function generateCacheHeaders() : array
+    {
+        // Unsupported cache_limiter
+        if (! isset(self::$supported_cache_limiters[$this->cacheLimiter])) {
+            return [];
+        }
+
+        // cache_limiter: 'nocache'
+        if ('nocache' === $this->cacheLimiter) {
+            return [
+                'Expires'       => self::CACHE_PAST_DATE,
+                'Cache-Control' => 'no-store, no-cache, must-revalidate',
+                'Pragma'        => 'no-cache',
+            ];
+        }
+
+        $maxAge       = 60 * $this->cacheExpire;
+        $lastModified = $this->getLastModified();
+
+        // cache_limiter: 'public'
+        if ('public' === $this->cacheLimiter) {
+            return [
+                'Expires'       => gmdate(self::HTTP_DATE_FORMAT, time() + $maxAge),
+                'Cache-Control' => sprintf('public, max-age=%d', $maxAge),
+                'Last-Modified' => $lastModified,
+            ];
+        }
+
+        // cache_limiter: 'private'
+        if ('private' === $this->cacheLimiter) {
+            return [
+                'Expires'       => self::CACHE_PAST_DATE,
+                'Cache-Control' => sprintf('private, max-age=%d', $maxAge),
+                'Last-Modified' => $lastModified,
+            ];
+        }
+
+        // last possible case, cache_limiter = 'private_no_expire'
+        return [
+            'Cache-Control' => sprintf('private, max-age=%d', $maxAge),
+            'Last-Modified' => $lastModified,
+        ];
+    }
+
+    /**
+     * Return the Last-Modified header line based on the request's script file
+     * modified time. If no script file could be derived from the request we use
+     * this class file modification time as fallback.
+     * @return string|false
+     */
+    private function getLastModified()
+    {
+        if ($this->scriptFile && is_file($this->scriptFile)) {
+            return gmdate(self::HTTP_DATE_FORMAT, filemtime($this->scriptFile));
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if the response already carries cache headers
+     */
+    private function responseAlreadyHasCacheHeaders(ResponseInterface $response) : bool
+    {
+        return (
+            $response->hasHeader('Expires')
+            || $response->hasHeader('Last-Modified')
+            || $response->hasHeader('Cache-Control')
+            || $response->hasHeader('Pragma')
+        );
+    }
+}
